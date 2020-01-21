b5995b384a9238db8dfb03399852fd69767a7b54
Moses Palacios <mospalacios@gmail.com>
2020-01-21 17:18:55 -0500

    installed necessary packages

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const fs = require('fs');
+const util = require("util");
+const inquire = require('inquire');
+const axios = require('axios');
 const convertFactory = require('electron-html-to');
+
+function promptUser() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "gitName",
+        message: "What is your Git Hub username?"
+      },
+      {
+        type: "input",
+        name: "favColor",
+        message: "What is your favorite color?"
+      },
+    ]);
+  }
+
+
