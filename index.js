48d4592a229a2c79d8e8679a2d6a486e9d75025b
Moses Palacios <mospalacios@gmail.com>
2020-01-22 17:51:09 -0500

    added css styling to the html file

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const fs = require('fs');
 const util = require("util");
 const inquirer = require('inquirer');
 const axios = require('axios');
-const electronPDF = require('electron-pdf');
+//onst electronPDF = require('electron-pdf');
+//const pdfGenerator = new electronPDF();
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
     return inquirer.prompt([
+        {
+            type: "input", 
+            name: "name", 
+            message: "What is your name"
+        },
       {
         type: "input",
         name: "gitName",
@@ -28,15 +34,16 @@ function promptUser() {
     <head>
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></div>
+      <link rel="stylesheet" href="./assets/style.css">
       <script src="https://kit.fontawesome.com/f674c96901.js" crossorigin="anonymous"></script>
       <title>Document</title>
     </head>
     <body>
       <div class="jumbotron">
           <div class="container rounded">
-              <img src="${gitInfo.data[0].owner.avatar_url}" alt="">
-              <h1 class="display-4 text-center">Hi! My name is </h1>
+              <div class="text-center"><img src="${gitInfo.data[0].owner.avatar_url}" alt="" class = "rounded-circle"></div>
+              <h1 class="display-4 text-center">Hi! My name is ${answers.name}</h1>
               <h3 class="text-center">Currently at ${userInfo.data.company}</h3>
               <a class="text-center" href="${mapsURL}"><h4><i class="fas fa-location-arrow"></i> ${userInfo.data.location}</h4></a>
               <a class="text-center" href="${userURL}"><h4><i class="fab fa-github"></i> GitHub</h4></a>
@@ -48,12 +55,12 @@ function promptUser() {
     <h4 class="text-center">${userInfo.data.bio}</h4>
     <div class="container">
         <div class="row mx-md-n5 my-4">
-            <div class="col px-md-5 text-center"><div class="p-3 border bg-light"><p>Public Repositories</p> <p>${gitInfo.data.length}</p></div></div>
-            <div class="col px-md-5 text-center"><div class="p-3 border bg-light"><p>Followers</p> <p>${followerResponse.data.length}</p></div></div>
+            <div class="col px-md-5 text-center "><div class="p-3 border" style="background-color: ${answers.favColor};"><p>Public Repositories</p> <p>${gitInfo.data.length}</p></div></div>
+            <div class="col px-md-5 text-center"><div class="p-3 border" style="background-color: ${answers.favColor};"><p>Followers</p> <p>${followerResponse.data.length}</p></div></div>
         </div>
         <div class="row mx-md-n5">
-            <div class="col px-md-5 text-center"><div class="p-3 border bg-light"><p>Git Hub Stars</p> <p>${starredResponse.data.length}</p></div></div>
-            <div class="col px-md-5 text-center"><div class="p-3 border bg-light"><p>Following</p> <p>${followingResponse.data.length}</p></div></div>
+            <div class="col px-md-5 text-center"><div class="p-3 border" style="background-color: ${answers.favColor};"><p>Git Hub Stars</p> <p>${starredResponse.data.length}</p></div></div>
+            <div class="col px-md-5 text-center"><div class="p-3 border" style="background-color: ${answers.favColor};"><p>Following</p> <p>${followingResponse.data.length}</p></div></div>
         </div>
 
   
@@ -85,6 +92,8 @@ async function getData(answers){
         const html = await generateHTML(gitInfo, followerResponse, followingResponse, starredResponse, userInfo, mapsURL, userURL, answers);
         await writeFileAsync("index.html", html);
         console.log("Successfully wrote to 'index.html' file");
+
+        //await pdfGenerator("index.html", "index.pdf")
         
     } catch (err) {
         console.log(err);
@@ -94,7 +103,4 @@ async function getData(answers){
 
 promptUser().then(function(answers){
     getData(answers);
-}).then(function(result){
-    pdfExport = new electronPDF();
-    pdfExport
-})
+});
