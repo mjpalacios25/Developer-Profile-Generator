7c3a144350a374804f81627ce506effbde49bf49
Moses Palacios <mospalacios@gmail.com>
2020-01-22 14:44:59 -0500

    created async function to run each axios call separately

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function promptUser() {
     ]);
   };
 
-  function generateHTML(answers) {
+  function generateHTML(infoResponse, followers, following, starred) {
     return `
     <!DOCTYPE html>
     <html lang="en">
@@ -35,7 +35,7 @@ function promptUser() {
     <body>
       <div class="jumbotron">
           <div class="container rounded">
-              <img src="" alt="">
+              <img src="${infoResponse.data[0].owner.avatar_url}" alt="">
               <h1 class="display-4 text-center">Hi! My name is ${answers.name}</h1>
               <p class="lead text-center">I am from ${answers.location}.</p>
               <h3 class="text-center">Currently at ${answers.place}</h3>
@@ -48,14 +48,15 @@ function promptUser() {
     </div>
     <h4 class="text-center">${answers.bio}</h4>
     <div class="container">
-      <div class="row mx-md-n5 my-4">
-          <div class="col px-md-5 text-center"><div class="p-3 border bg-light">Custom column padding</div></div>
-          <div class="col px-md-5 text-center"><div class="p-3 border bg-light">Custom column padding</div></div>
+        <div class="row mx-md-n5 my-4">
+            <div class="col px-md-5 text-center"><div class="p-3 border bg-light"><p>Public Repositories</p> <p>${answers.data.length}</p></div></div>
+            <div class="col px-md-5 text-center"><div class="p-3 border bg-light"><p>Followers</p> <p>${answers.data.length}</p></div></div>
         </div>
         <div class="row mx-md-n5">
-          <div class="col px-md-5 text-center"><div class="p-3 border bg-light">Custom column padding</div></div>
-          <div class="col px-md-5 text-center"><div class="p-3 border bg-light">Custom column padding</div></div>
+            <div class="col px-md-5 text-center"><div class="p-3 border bg-light"><p>Git Hub Stars</p> <p>${answers.data.length}</p></div></div>
+            <div class="col px-md-5 text-center"><div class="p-3 border bg-light"><p>Following</p> <p>${answers.data.length}</p></div></div>
         </div>
+
   
     </div>
   
@@ -63,13 +64,79 @@ function promptUser() {
     </html>`;
   };
 
+async function getData(answers){
+    try{
+        const queryURL = `https://api.github.com/users/${answers.gitName}/repos?per_page=100`;
+        const followerURL = `https://api.github.com/users/${answers.gitName}/followers`;
+        const followingUrl = `https://api.github.com/users/${answers.gitName}/following`;
+        const starredURL = `https://api.github.com/users/${answers.gitName}/starred`
+        const profileURL = `https://api.github.com/users/${answers.gitName}`
+    
+        const infoResponse =  await axios.get(queryURL);
+        const followerResponse =  await axios.get(followerURL);
+        const followingResponse =  await axios.get(followingUrl);
+        const starredResponse =  await axios.get(starredURL);
+        const location = await axios.get(profileURL)
 
-
-promptUser().then((answers) => {
-
-    const queryURL = `https://api.github.com/users/${answers.gitName}/repos?per_page=100`;
-    axios.get(queryURL).then(function(response){
-        console.log(queryURL);
+        //const html = generateHTML();
+        //.then(function(response){
+            //console.log(response.data);
+            // console.log(response.data.length);
+            // console.log(response.data[0].owner.avatar_url);
+            // console.log(queryURL);
+            //console.log(response.data.length);
+        //});
         
-    })
-});
+        
+    } catch (err) {
+        console.log(err);
+      }
+}
+
+
+promptUser().then(function(answers){
+    getData(answers);
+})
+
+// promptUser().then((answers) => {
+
+//     const queryURL = `https://api.github.com/users/${answers.gitName}/repos?per_page=100`;
+//     const followerURL = `https://api.github.com/users/${answers.gitName}/followers`;
+//     const followingUrl = `https://api.github.com/users/${answers.gitName}/following`;
+//     const starredURL = `https://api.github.com/users/${answers.gitName}/starred`;
+    
+//     axios.get(queryURL).then(function(response){
+//         //console.log(response.data);
+//         // console.log(response.data.length);
+//         // console.log(response.data[0].owner.avatar_url);
+//         // console.log(queryURL);
+//         var thing1 = response.data.length;
+        
+//     });
+    
+    
+    
+// })
+// .then(function(){
+//     axios.get(followerURL).then(function(follow){
+//                 var thing2 = follow.data.length;
+//                 return thing2
+//             });
+// })
+
+
+
+
+    // var followers = axios.get(followerURL).then(function(response){
+    //     console.log(response.data.length)
+    // });
+    // var following = axios.get(followingUrl).then(function(response){
+    //     console.log(response.data.length)
+    // });
+    // var starred = axios.get(starredURL).then(function(response){
+    //     console.log(response.data.length)
+    // });
+
+    
+    
+
