971a3a4fdc0cbb6b008ecc4b81fe5120903e8116
Moses Palacios <mospalacios@gmail.com>
2020-01-22 16:46:41 -0500

    integreated fields into the html file... working on figuring out electron pdf

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const util = require("util");
 const inquirer = require('inquirer');
 const axios = require('axios');
-// const electronPDF = require('electron-pdf');
+const electronPDF = require('electron-pdf');
 
-// const writeFileAsync = util.promisify(fs.writeFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
     return inquirer.prompt([
@@ -21,7 +21,7 @@ function promptUser() {
     ]);
   };
 
-  function generateHTML(infoResponse, followers, following, starred) {
+  function generateHTML(gitInfo, followerResponse, followingResponse, starredResponse, userInfo, mapsURL, userURL, answers) {
     return `
     <!DOCTYPE html>
     <html lang="en">
@@ -35,26 +35,25 @@ function promptUser() {
     <body>
       <div class="jumbotron">
           <div class="container rounded">
-              <img src="${infoResponse.data[0].owner.avatar_url}" alt="">
-              <h1 class="display-4 text-center">Hi! My name is ${answers.name}</h1>
-              <p class="lead text-center">I am from ${answers.location}.</p>
-              <h3 class="text-center">Currently at ${answers.place}</h3>
-              <a class="text-center" href=""><h4><i class="fas fa-location-arrow"></i> Where I am</h4></a>
-              <a class="text-center" href=""><h4><i class="fab fa-github"></i> ${answers.github}</h4></a>
-              <a class="text-center" href=""><h4><i class="fas fa-blog"></i> ${answers.blog}</h4></a>
-              <h3 class="text-center">My GitHub username is </h3>
-              <h3 class="text-center">Currently at ${answers.place}</h3>
+              <img src="${gitInfo.data[0].owner.avatar_url}" alt="">
+              <h1 class="display-4 text-center">Hi! My name is </h1>
+              <h3 class="text-center">Currently at ${userInfo.data.company}</h3>
+              <a class="text-center" href="${mapsURL}"><h4><i class="fas fa-location-arrow"></i> ${userInfo.data.location}</h4></a>
+              <a class="text-center" href="${userURL}"><h4><i class="fab fa-github"></i> GitHub</h4></a>
+              <a class="text-center" href=""><h4><i class="fas fa-blog"></i> Blog</h4></a>
+              <h3 class="text-center">My GitHub username is ${answers.gitName}</h3>
+              <h3 class="text-center">Currently at ${userInfo.data.company}</h3>
           </div>
     </div>
-    <h4 class="text-center">${answers.bio}</h4>
+    <h4 class="text-center">${userInfo.data.bio}</h4>
     <div class="container">
         <div class="row mx-md-n5 my-4">
-            <div class="col px-md-5 text-center"><div class="p-3 border bg-light"><p>Public Repositories</p> <p>${answers.data.length}</p></div></div>
-            <div class="col px-md-5 text-center"><div class="p-3 border bg-light"><p>Followers</p> <p>${answers.data.length}</p></div></div>
+            <div class="col px-md-5 text-center"><div class="p-3 border bg-light"><p>Public Repositories</p> <p>${gitInfo.data.length}</p></div></div>
+            <div class="col px-md-5 text-center"><div class="p-3 border bg-light"><p>Followers</p> <p>${followerResponse.data.length}</p></div></div>
         </div>
         <div class="row mx-md-n5">
-            <div class="col px-md-5 text-center"><div class="p-3 border bg-light"><p>Git Hub Stars</p> <p>${answers.data.length}</p></div></div>
-            <div class="col px-md-5 text-center"><div class="p-3 border bg-light"><p>Following</p> <p>${answers.data.length}</p></div></div>
+            <div class="col px-md-5 text-center"><div class="p-3 border bg-light"><p>Git Hub Stars</p> <p>${starredResponse.data.length}</p></div></div>
+            <div class="col px-md-5 text-center"><div class="p-3 border bg-light"><p>Following</p> <p>${followingResponse.data.length}</p></div></div>
         </div>
 
   
@@ -70,23 +69,22 @@ async function getData(answers){
         const followerURL = `https://api.github.com/users/${answers.gitName}/followers`;
         const followingUrl = `https://api.github.com/users/${answers.gitName}/following`;
         const starredURL = `https://api.github.com/users/${answers.gitName}/starred`
-        const profileURL = `https://api.github.com/users/${answers.gitName}`
+        const profileURL = `https://api.github.com/users/${answers.gitName}`;
+        const userURL = `https://github.com/${answers.gitName}`
+        
     
-        const infoResponse =  await axios.get(queryURL);
+        const gitInfo =  await axios.get(queryURL);
         const followerResponse =  await axios.get(followerURL);
         const followingResponse =  await axios.get(followingUrl);
         const starredResponse =  await axios.get(starredURL);
-        const location = await axios.get(profileURL)
-
-        //const html = generateHTML();
-        //.then(function(response){
-            //console.log(response.data);
-            // console.log(response.data.length);
-            // console.log(response.data[0].owner.avatar_url);
-            // console.log(queryURL);
-            //console.log(response.data.length);
-        //});
+        const userInfo = await axios.get(profileURL)
+        const mapsURL = `https://www.google.com/maps/place/${userInfo.data.location}`
         
+        console.log(mapsURL);
+
+        const html = await generateHTML(gitInfo, followerResponse, followingResponse, starredResponse, userInfo, mapsURL, userURL, answers);
+        await writeFileAsync("index.html", html);
+        console.log("Successfully wrote to 'index.html' file");
         
     } catch (err) {
         console.log(err);
@@ -96,47 +94,7 @@ async function getData(answers){
 
 promptUser().then(function(answers){
     getData(answers);
+}).then(function(result){
+    pdfExport = new electronPDF();
+    pdfExport
 })
-
-// promptUser().then((answers) => {
-
-//     const queryURL = `https://api.github.com/users/${answers.gitName}/repos?per_page=100`;
-//     const followerURL = `https://api.github.com/users/${answers.gitName}/followers`;
-//     const followingUrl = `https://api.github.com/users/${answers.gitName}/following`;
-//     const starredURL = `https://api.github.com/users/${answers.gitName}/starred`;
-    
-//     axios.get(queryURL).then(function(response){
-//         //console.log(response.data);
-//         // console.log(response.data.length);
-//         // console.log(response.data[0].owner.avatar_url);
-//         // console.log(queryURL);
-//         var thing1 = response.data.length;
-        
-//     });
-    
-    
-    
-// })
-// .then(function(){
-//     axios.get(followerURL).then(function(follow){
-//                 var thing2 = follow.data.length;
-//                 return thing2
-//             });
-// })
-
-
-
-
-    // var followers = axios.get(followerURL).then(function(response){
-    //     console.log(response.data.length)
-    // });
-    // var following = axios.get(followingUrl).then(function(response){
-    //     console.log(response.data.length)
-    // });
-    // var starred = axios.get(starredURL).then(function(response){
-    //     console.log(response.data.length)
-    // });
-
-    
-    
-
